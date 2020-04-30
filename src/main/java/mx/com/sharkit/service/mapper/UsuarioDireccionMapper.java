05d9b039f3ef6495d93b8f5fdf7922085eeb2121
Adrian Salgado <asalgado@santander.com.mx>
2020-04-30 16:00:15 -0500

    issue direcciones

--- src/main/java/mx/com/sharkit/service/mapper/UsuarioDireccionMapper.java
+++ src/main/java/mx/com/sharkit/service/mapper/UsuarioDireccionMapper.java
@@ -1,6 +1,7 @@
 package mx.com.sharkit.service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import mx.com.sharkit.domain.UsuarioDireccion;
 import mx.com.sharkit.domain.UsuarioImagen;
@@ -13,8 +14,12 @@ import mx.com.sharkit.service.dto.UsuarioImagenDTO;
 @Mapper(componentModel = "spring", uses = {UserMapper.class, DireccionMapper.class})
 public interface UsuarioDireccionMapper extends EntityMapper<UsuarioDireccionDTO, UsuarioDireccion> {
 
+    @Mapping(source = "usuario.id", target = "usuarioId")
+    @Mapping(source = "direccion.id", target = "direccionId")
     UsuarioDireccionDTO toDto(UsuarioDireccion usuarioImagen);
 
+    @Mapping(source = "usuarioId", target = "usuario")
+    @Mapping(source = "direccionId", target = "direccion")
     UsuarioDireccion toEntity(UsuarioDireccionDTO usuarioImagenDTO);
 
     default UsuarioDireccion fromId(Long id) {
