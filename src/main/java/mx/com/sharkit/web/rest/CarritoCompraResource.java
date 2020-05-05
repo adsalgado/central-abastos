69c15de439e09a615fbe0b0ff1ee02f9db6ee675
Adrian Salgado <asalgado@santander.com.mx>
2020-05-05 13:27:01 -0500

    Carrito de compras por proveedor

--- src/main/java/mx/com/sharkit/web/rest/CarritoCompraResource.java
+++ src/main/java/mx/com/sharkit/web/rest/CarritoCompraResource.java
@@ -1,9 +1,13 @@
 package mx.com.sharkit.web.rest;
 
+import java.math.BigDecimal;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -27,6 +31,7 @@ import mx.com.sharkit.domain.User;
 import mx.com.sharkit.service.CarritoCompraService;
 import mx.com.sharkit.service.UserService;
 import mx.com.sharkit.service.dto.CarritoCompraDTO;
+import mx.com.sharkit.service.dto.CarritoComprasProveedorDTO;
 import mx.com.sharkit.web.rest.errors.BadRequestAlertException;
 
 /**
@@ -36,133 +41,193 @@ import mx.com.sharkit.web.rest.errors.BadRequestAlertException;
 @RequestMapping("/api")
 public class CarritoCompraResource {
 
-    private final Logger log = LoggerFactory.getLogger(CarritoCompraResource.class);
+	private final Logger log = LoggerFactory.getLogger(CarritoCompraResource.class);
 
-    private static final String ENTITY_NAME = "carritoCompra";
+	private static final String ENTITY_NAME = "carritoCompra";
 
-    @Value("${jhipster.clientApp.name}")
-    private String applicationName;
+	@Value("${jhipster.clientApp.name}")
+	private String applicationName;
 
-    private final CarritoCompraService carritoCompraService;
-    
-    private final UserService userService;
+	private final CarritoCompraService carritoCompraService;
 
-    public CarritoCompraResource(CarritoCompraService carritoCompraService, UserService userService) {
-        this.carritoCompraService = carritoCompraService;
-        this.userService = userService;
-    }
+	private final UserService userService;
 
-    /**
-     * {@code POST  /carrito-compras} : Create a new carritoCompra.
-     *
-     * @param carritoCompraDTO the carritoCompraDTO to create.
-     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new carritoCompraDTO, or with status {@code 400 (Bad Request)} if the carritoCompra has already an ID.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
-     */
-    @PostMapping("/carrito-compras")
-    public ResponseEntity<CarritoCompraDTO> createCarritoCompra(@Valid @RequestBody CarritoCompraDTO carritoCompraDTO) throws URISyntaxException {
-        log.debug("REST request to save CarritoCompra : {}", carritoCompraDTO);
-        if (carritoCompraDTO.getId() != null) {
-            throw new BadRequestAlertException("A new carritoCompra cannot already have an ID", ENTITY_NAME, "idexists");
-        }
-        Optional<User> user = userService.getUserWithAuthorities();
-        Long clienteId =  user.isPresent() ? user.get().getId() : 0L;
-        if (clienteId == 0) {
-            throw new BadRequestAlertException("El cliente es requerido", ENTITY_NAME, "idnull");
-        }
-        Optional <CarritoCompraDTO> optCarritoDto = carritoCompraService.findOneClienteIdAndProductoProveedorId(clienteId, carritoCompraDTO.getProductoProveedorId());
-        if (optCarritoDto.isPresent()) {
-            throw new BadRequestAlertException("Ya existe este producto en el carrito.", ENTITY_NAME, "idexists");        	
-        }
+	public CarritoCompraResource(CarritoCompraService carritoCompraService, UserService userService) {
+		this.carritoCompraService = carritoCompraService;
+		this.userService = userService;
+	}
 
-        carritoCompraDTO.setClienteId(clienteId);
-        carritoCompraDTO.setFechaAlta(LocalDateTime.now());
-        
-        CarritoCompraDTO result = carritoCompraService.save(carritoCompraDTO);
-        return ResponseEntity.created(new URI("/api/carrito-compras/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
-            .body(result);
-    }
+	/**
+	 * {@code POST  /carrito-compras} : Create a new carritoCompra.
+	 *
+	 * @param carritoCompraDTO the carritoCompraDTO to create.
+	 * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with
+	 *         body the new carritoCompraDTO, or with status
+	 *         {@code 400 (Bad Request)} if the carritoCompra has already an ID.
+	 * @throws URISyntaxException if the Location URI syntax is incorrect.
+	 */
+	@PostMapping("/carrito-compras")
+	public ResponseEntity<CarritoCompraDTO> createCarritoCompra(@Valid @RequestBody CarritoCompraDTO carritoCompraDTO)
+			throws URISyntaxException {
+		log.debug("REST request to save CarritoCompra : {}", carritoCompraDTO);
+		if (carritoCompraDTO.getId() != null) {
+			throw new BadRequestAlertException("A new carritoCompra cannot already have an ID", ENTITY_NAME,
+					"idexists");
+		}
+		Optional<User> user = userService.getUserWithAuthorities();
+		Long clienteId = user.isPresent() ? user.get().getId() : 0L;
+		if (clienteId == 0) {
+			throw new BadRequestAlertException("El cliente es requerido", ENTITY_NAME, "idnull");
+		}
+		Optional<CarritoCompraDTO> optCarritoDto = carritoCompraService
+				.findOneClienteIdAndProductoProveedorId(clienteId, carritoCompraDTO.getProductoProveedorId());
+		if (optCarritoDto.isPresent()) {
+			throw new BadRequestAlertException("Ya existe este producto en el carrito.", ENTITY_NAME, "idexists");
+		}
 
-    /**
-     * {@code PUT  /carrito-compras} : Updates an existing carritoCompra.
-     *
-     * @param carritoCompraDTO the carritoCompraDTO to update.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated carritoCompraDTO,
-     * or with status {@code 400 (Bad Request)} if the carritoCompraDTO is not valid,
-     * or with status {@code 500 (Internal Server Error)} if the carritoCompraDTO couldn't be updated.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
-     */
-    @PutMapping("/carrito-compras")
-    public ResponseEntity<CarritoCompraDTO> updateCarritoCompra(@Valid @RequestBody CarritoCompraDTO carritoCompraDTO) throws URISyntaxException {
-        log.debug("REST request to update CarritoCompra : {}", carritoCompraDTO);
-        
-        Optional<User> user = userService.getUserWithAuthorities();
-        Long clienteId =  user.isPresent() ? user.get().getId() : 0L;
-        if (clienteId == 0) {
-            throw new BadRequestAlertException("El cliente es requerido", ENTITY_NAME, "idexists");
-        }
-        CarritoCompraDTO result = carritoCompraDTO;
-        
-        Optional <CarritoCompraDTO> optCarritoDto = carritoCompraService.findOneClienteIdAndProductoProveedorId(clienteId, carritoCompraDTO.getProductoProveedorId());
-        if (optCarritoDto.isPresent()) {
-        	CarritoCompraDTO dto = optCarritoDto.get();
-        	dto.setCantidad(carritoCompraDTO.getCantidad());
-        	result = carritoCompraService.save(dto);
-        }
-        
-    	if (result.getId() == null) {
-            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
-        }
+		carritoCompraDTO.setClienteId(clienteId);
+		carritoCompraDTO.setFechaAlta(LocalDateTime.now());
 
-         
-        return ResponseEntity.ok()
-            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
-            .body(result);
-    }
+		CarritoCompraDTO result = carritoCompraService.save(carritoCompraDTO);
+		return ResponseEntity
+				.created(new URI("/api/carrito-compras/" + result.getId())).headers(HeaderUtil
+						.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
+				.body(result);
+	}
 
-    /**
-     * {@code GET  /carrito-compras} : get all the carritoCompras by clienteId.
-     *
+	/**
+	 * {@code PUT  /carrito-compras} : Updates an existing carritoCompra.
+	 *
+	 * @param carritoCompraDTO the carritoCompraDTO to update.
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body
+	 *         the updated carritoCompraDTO, or with status
+	 *         {@code 400 (Bad Request)} if the carritoCompraDTO is not valid, or
+	 *         with status {@code 500 (Internal Server Error)} if the
+	 *         carritoCompraDTO couldn't be updated.
+	 * @throws URISyntaxException if the Location URI syntax is incorrect.
+	 */
+	@PutMapping("/carrito-compras")
+	public ResponseEntity<CarritoCompraDTO> updateCarritoCompra(@Valid @RequestBody CarritoCompraDTO carritoCompraDTO)
+			throws URISyntaxException {
+		log.debug("REST request to update CarritoCompra : {}", carritoCompraDTO);
 
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of carritoCompras in body.
-     */
-    @GetMapping("/carrito-compras")
-    public List<CarritoCompraDTO> getAllCarritoComprasByClienteId() {
-        log.debug("REST request to get all CarritoCompras");
-        Optional<User> user = userService.getUserWithAuthorities();
-        Long clienteId =  user.isPresent() ? user.get().getId() : 0L;
-        return carritoCompraService.findAllByClienteId(clienteId);
-    }
-    /**
-     * {@code GET  /carrito-compras/:id} : get the "id" carritoCompra.
-     *
-     * @param id the id of the carritoCompraDTO to retrieve.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the carritoCompraDTO, or with status {@code 404 (Not Found)}.
-     */
-    @GetMapping("/carrito-compras/{id}")
-    public ResponseEntity<CarritoCompraDTO> getCarritoCompra(@PathVariable Long id) {
-        log.debug("REST request to get CarritoCompra : {}", id);
-        Optional<CarritoCompraDTO> carritoCompraDTO = carritoCompraService.findOne(id);
-        return ResponseUtil.wrapOrNotFound(carritoCompraDTO);
-    }
+		Optional<User> user = userService.getUserWithAuthorities();
+		Long clienteId = user.isPresent() ? user.get().getId() : 0L;
+		if (clienteId == 0) {
+			throw new BadRequestAlertException("El cliente es requerido", ENTITY_NAME, "idexists");
+		}
+		CarritoCompraDTO result = carritoCompraDTO;
 
-    /**
-     * {@code DELETE  /carrito-compras/:id} : delete the "id" carritoCompra.
-     *
-     * @param id the id of the carritoCompraDTO to delete.
-     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
-     */
-    @DeleteMapping("/carrito-compras/{id}")
-    public ResponseEntity<Void> deleteCarritoCompra(@PathVariable Long id) {
-        log.debug("REST request to delete CarritoCompra : {}", id);
-        Optional<User> user = userService.getUserWithAuthorities();
-        Long clienteId =  user.isPresent() ? user.get().getId() : 0L;
-        if (clienteId == 0) {
-            throw new BadRequestAlertException("El cliente es requerido", ENTITY_NAME, "idnull");
-        }
-        carritoCompraService.delete(id);
-        return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString())).build();
-    }
+		Optional<CarritoCompraDTO> optCarritoDto = carritoCompraService
+				.findOneClienteIdAndProductoProveedorId(clienteId, carritoCompraDTO.getProductoProveedorId());
+		if (optCarritoDto.isPresent()) {
+			CarritoCompraDTO dto = optCarritoDto.get();
+			dto.setCantidad(carritoCompraDTO.getCantidad());
+			result = carritoCompraService.save(dto);
+		}
+
+		if (result.getId() == null) {
+			throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+		}
+
+		return ResponseEntity.ok().headers(
+				HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
+				.body(result);
+	}
+
+	/**
+	 * {@code GET  /carrito-compras} : get all the carritoCompras by clienteId.
+	 *
+	 * 
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list
+	 *         of carritoCompras in body.
+	 */
+	@GetMapping("/carrito-compras")
+	public List<CarritoCompraDTO> getAllCarritoComprasByClienteId() {
+		log.debug("REST request to get all CarritoCompras");
+		Optional<User> user = userService.getUserWithAuthorities();
+		Long clienteId = user.isPresent() ? user.get().getId() : 0L;
+		if (clienteId == 0) {
+			throw new BadRequestAlertException("El cliente es requerido", ENTITY_NAME, "idnull");
+		}
+		return carritoCompraService.findAllByClienteId(clienteId);
+	}
+
+	/**
+	 * {@code GET  /carrito-compras/:id} : get the "id" carritoCompra.
+	 *
+	 * @param id the id of the carritoCompraDTO to retrieve.
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body
+	 *         the carritoCompraDTO, or with status {@code 404 (Not Found)}.
+	 */
+	@GetMapping("/carrito-compras/{id}")
+	public ResponseEntity<CarritoCompraDTO> getCarritoCompra(@PathVariable Long id) {
+		log.debug("REST request to get CarritoCompra : {}", id);
+		Optional<CarritoCompraDTO> carritoCompraDTO = carritoCompraService.findOne(id);
+		return ResponseUtil.wrapOrNotFound(carritoCompraDTO);
+	}
+
+	/**
+	 * {@code DELETE  /carrito-compras/:id} : delete the "id" carritoCompra.
+	 *
+	 * @param id the id of the carritoCompraDTO to delete.
+	 * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+	 */
+	@DeleteMapping("/carrito-compras/{id}")
+	public ResponseEntity<Void> deleteCarritoCompra(@PathVariable Long id) {
+		log.debug("REST request to delete CarritoCompra : {}", id);
+		Optional<User> user = userService.getUserWithAuthorities();
+		Long clienteId = user.isPresent() ? user.get().getId() : 0L;
+		if (clienteId == 0) {
+			throw new BadRequestAlertException("El cliente es requerido", ENTITY_NAME, "idnull");
+		}
+		carritoCompraService.delete(id);
+		return ResponseEntity.noContent()
+				.headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString()))
+				.build();
+	}
+
+	/**
+	 * {@code GET  /carrito-compras-proveedor} : get all the carritoCompras by
+	 * clienteId.
+	 *
+	 * 
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list
+	 *         of carritoCompras in body.
+	 */
+	@GetMapping("/carrito-compras-proveedor")
+	public List<CarritoComprasProveedorDTO> getAllCarritoOrderByProveedor() {
+		log.debug("REST request to get all CarritoCompras");
+		Optional<User> user = userService.getUserWithAuthorities();
+		Long clienteId = user.isPresent() ? user.get().getId() : 0L;
+		if (clienteId == 0) {
+			throw new BadRequestAlertException("El cliente es requerido", ENTITY_NAME, "idnull");
+		}
+		
+		Map<Long, CarritoComprasProveedorDTO> mapProveedores = new HashMap<>();
+
+		List<CarritoCompraDTO> listCarrito = carritoCompraService.findAllByClienteId(clienteId);
+		for (CarritoCompraDTO carritoCompraDTO : listCarrito) {
+			Long proveedorId = carritoCompraDTO.getProductoProveedor().getProveedorId();
+			if (!mapProveedores.containsKey(proveedorId)) {
+				CarritoComprasProveedorDTO carritoProveedor = new CarritoComprasProveedorDTO();
+				carritoProveedor.setListCarrito(new ArrayList<>());
+				carritoProveedor.setProveedor(carritoCompraDTO.getProductoProveedor().getProveedor());
+				carritoProveedor.setTotal(BigDecimal.ZERO);
+				mapProveedores.put(proveedorId, carritoProveedor);
+			}
+			CarritoComprasProveedorDTO carritoProveedor = mapProveedores.get(proveedorId);
+			carritoProveedor.getListCarrito().add(carritoCompraDTO);
+			carritoProveedor.setTotal(carritoProveedor.getTotal().add(carritoCompraDTO.getPrecio().multiply(carritoCompraDTO.getCantidad())));
+		}
+
+		List<CarritoComprasProveedorDTO> listCarritoProveedorDTO = new ArrayList<>();
+		mapProveedores.keySet().forEach(provId -> {
+			listCarritoProveedorDTO.add(mapProveedores.get(provId));
+		});
+			
+		return listCarritoProveedorDTO;
+
+	}
 
 }
