d2596743d9ee7468e453b9a980250f4778375f47
Adrian Salgado <asalgado@santander.com.mx>
2020-05-12 18:11:08 -0500

    Cambios stripe

--- src/main/java/mx/com/sharkit/service/impl/StripeServiceImpl.java
+++ src/main/java/mx/com/sharkit/service/impl/StripeServiceImpl.java
@@ -1,10 +1,15 @@
 package mx.com.sharkit.service.impl;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
 
@@ -13,11 +18,21 @@ import mx.com.sharkit.service.dto.ChargeRequestDTO;
 
 @Service
 public class StripeServiceImpl implements StripeService {
+	
+	@Value("${stripe.secret.key}")
+    private String secretKey;
+	
+	@PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
 
 	@Override
 	public Charge charge(ChargeRequestDTO chargeRequest) throws StripeException {
 		Map<String, Object> chargeParams = new HashMap<>();
-		chargeParams.put("amount", chargeRequest.getAmount());
+		// El monto debe ser en centavos, según la documentación
+		int amount = (chargeRequest.getAmount().multiply(new BigDecimal(100))).intValue();
+		chargeParams.put("amount", amount);
 		chargeParams.put("currency", chargeRequest.getCurrency());
 		chargeParams.put("description", chargeRequest.getDescription());
 		chargeParams.put("source", chargeRequest.getStripeToken());
